2f5da4c213b18108fe8c156bf71a9c78dfaf2313
Filip Lazovski <filip_lazovski@hotmail.com>
2022-04-11 22:17:39 +0200

    more

--- functions/node-fetch/node-fetch.js
+++ functions/node-fetch/node-fetch.js
@@ -5,10 +5,11 @@ const handler = async function () {
     let config = {
       headers: {
         Accept: 'application/json',
-        Authorization: "Bearer " + process.env.REACT_TWITTER_KEY
+        Authorization: "Bearer AAAAAAAAAAAAAAAAAAAAAL2JbQEAAAAA%2FGjuMZ22HUhgXcprTkzpr5V1%2FKk%3DVP8uGcyBbucwDaOJX3mVJupSvOVKRxRUWWeFSpZDPD8e6yUHA8"
       }
     };
     const response = await fetch('https://api.twitter.com/2/users/1648317175/tweets', config)
+    console.log('RESPONSOT', response);
     if (!response.ok) {
       // NOT res.status >= 200 && res.status < 300
       return { statusCode: response.status, body: response.statusText }
